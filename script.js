8d2c90b84b24aeed036ffc2db5d55ac6764fe2fa
amir seifollahi <amirseifollahiii@gmail.com>
2021-08-04 16:54:57 +0430

    add clients part

---
+++ script.js
@@ -0,0 +1,71 @@
+"use strict";
+
+// const html =
+const clientContainer = document.querySelector(".clients");
+const users = [
+  {
+    image:
+      "http://www.templatewire.com/preview/interact/img/testimonials/01.jpg",
+    name: "john doe",
+    comment:
+      '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis bibendum dolor feugiat at."',
+  },
+  {
+    image:
+      "http://www.templatewire.com/preview/interact/img/testimonials/02.jpg",
+    name: "johnathan doe",
+    comment:
+      '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis."',
+  },
+  {
+    image:
+      "http://www.templatewire.com/preview/interact/img/testimonials/03.jpg",
+    name: "john doe",
+    comment:
+      '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis bibendum dolor feugiat at."',
+  },
+  {
+    image:
+      "http://www.templatewire.com/preview/interact/img/testimonials/04.jpg",
+    name: "johnathan doe",
+    comment:
+      '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis."',
+  },
+  {
+    image:
+      "http://www.templatewire.com/preview/interact/img/testimonials/05.jpg",
+    name: "john doe",
+    comment:
+      '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis bibendum dolor feugiat at."',
+  },
+  {
+    image:
+      "http://www.templatewire.com/preview/interact/img/testimonials/06.jpg",
+    name: "johnathan doe",
+    comment:
+      '"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis."',
+  },
+];
+
+const userCreator = function (arr) {
+  arr.forEach((el) => {
+    const html = `
+        <div class="client">
+        <div class="client-avatar">
+          <img
+            src=${el.image}
+            alt=""
+          />
+        </div>
+        <div class="clent-info">
+          <div class="client-comment">${el.comment}</div>
+          <div class="client-name">
+            <p>- ${el.name}</p>
+          </div>
+        </div>
+        </div>`;
+    clientContainer.insertAdjacentHTML("beforeend", html);
+  });
+};
+
+userCreator(users);
